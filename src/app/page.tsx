12165180c4e4f2e84c2f2f02eac5a0386773dae3
IMOURI <whymouri@gmail.com>
2025-08-06 04:30:55 -0700

    level 33

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -190,9 +190,12 @@ export default async function HomePage() {
                   </div>
                 </div>
                 {/* Enamad Trust Seal */}
-                <div className="flex justify-center mt-2">
+                <div className="flex justify-center mt-2 gap-4">
                   <a referrerPolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=633944&Code=jWDKWWr7vUb90Ux7m6wx52M9Xoj0PYXz">
                     <img referrerPolicy="origin" src="https://trustseal.enamad.ir/logo.aspx?id=633944&Code=jWDKWWr7vUb90Ux7m6wx52M9Xoj0PYXz" alt="enamad" style={{cursor:'pointer'}} />
+                  </a>
+                  <a referrerPolicy="origin" target="_blank" href="https://logo.samandehi.ir/Verify.aspx?id=391969&p=xlaopfvlrfthpfvlgvkapfvl">
+                    <img referrerPolicy="origin" id="rgvjsizpjxlzsizpfukzsizp" style={{cursor:'pointer'}} alt="logo-samandehi" src="https://logo.samandehi.ir/logo.aspx?id=391969&p=qftibsiynbpdbsiywlbqbsiy" />
                   </a>
                 </div>
 
